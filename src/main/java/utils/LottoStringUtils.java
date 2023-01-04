2c1277378648dbd531f4f9029ac0b993b41eda0f
엄태호 <qqsscc7342@gmail.com>
2023-01-04 18:48:49 +0900

    feat: Lotto StringUtils 구현

--- src/main/java/utils/LottoStringUtils.java
+++ src/main/java/utils/LottoStringUtils.java
@@ -1,2 +1,18 @@
-package utils;public class LottoStringUtils {
+package utils;
+
+import domain.lotto.LottoNumber;
+import domain.lotto.LottoNumbers;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class LottoStringUtils {
+    private static final String LOTTO_NUMBER_DELIMITER = ", ";
+
+    public static LottoNumbers stringToLottoNumbers(String input) {
+        return new LottoNumbers(
+                Arrays.stream(input.split(LOTTO_NUMBER_DELIMITER))
+                        .map(LottoNumber::of)
+                        .collect(Collectors.toList()));
+    }
 }
